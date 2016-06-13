90de1a195305f8944c5751a35e260d7276c13fb2
Alex Shumilov <ashumilov@pivotal.io>
2016-06-13 09:26:41 -0400

    start spaces, orgs api calls

--- api/cloud_controller_test.go
+++ api/cloud_controller_test.go
@@ -2,15 +2,10 @@ package api
 
 import (
 	"testing"
-	//"github.com/pivotalservices/_app-usage-nozzle/usageevents"
-	// "fmt"
-	//"encoding/json"
-	//"time"
-	//"github.com/davecgh/go-spew/spew"
-	//"github.com/pivotalservices/app-usage-nozzle/usageevents"
 	"app-usage-nozzle/domain"
+	"os"
+	"github.com/jtgammon/go-cfclient"
 	"fmt"
-	"time"
 )
 
 
@@ -18,10 +13,36 @@ var AppDetails = make(map[string]domain.App)
 
 func TestReverse(t *testing.T) {
 
-	now := time.Now()
+	c := cfclient.Config{
+		ApiAddress:        "https://api.run.haas-41.pez.pivotal.io",
+		Username:          "admin",
+		Password:          "cb0a40f8d6360eaed442",
+		SkipSslValidation: true,
+	}
 
-	fmt.Println("now:", now)
+	logger.Println("Processing Cloud Controller call to " + os.Getenv("API_ENDPOINT"))
+	client, _ = cfclient.NewClient(&c)
 
-	then := now.Add(-10 * time.Minute)
-	fmt.Println("10 minutes ago:", then)
+
+	orgs, _ := client.ListOrgs()
+	var o []Entity // == nil
+	for idx := range orgs {
+		org := Entity{Name:orgs[idx].Name, Guid:orgs[idx].Guid}
+		o = append(o, org)
+	}
+	logger.Println(fmt.Sprintf("Org name %s", o))
+
+
+	spaces, _ := client.ListSpaces()
+	for idx := range spaces {
+		org := Entity{Name:spaces[idx].Name, Guid:spaces[idx].Guid}
+		o = append(o, org)
+	}
+	logger.Println(fmt.Sprintf("Space name %s", o))
+
+}
+
+type Entity struct {
+	Name	string	`json:"name"`
+	Guid	string	`json:"guid"`
 }
